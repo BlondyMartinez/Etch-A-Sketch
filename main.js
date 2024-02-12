fb899d20f9a80e0a66414eb3ab2cd1abe2aa56c7
BlondyMartinez <blondymartinezm@gmail.com>
2024-02-12 04:29:23 +0100

    functional dynamic grid

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-function grid16x16() {
-    let container = document.querySelector('#grid')
-    let cell = document.createElement('div');
+function grid(cellsPerSide) {
+    let container = document.querySelector('#grid');
 
-    container.style.height = parseFloat(window.innerWidth * 0.8) + 'px';
-    cell.style.width = parseFloat((window.innerWidth * .8) / 16) + 'px';
-    cell.style.height = parseFloat((window.innerWidth * .8) / 16) + 'px';
+    let gridWidth = (window.innerWidth * 0.8 / (16/9));
+    let cellWidth = gridWidth / cellsPerSide;
 
-    let row = document.createElement('div');
-    row.style.display = 'flex';
+    container.style.height = gridWidth + 'px';
 
-    for(let i = 0; i < 16; i++) {
-        for(let j = 0; j < 16; j++){
+    for(let i = 0; i < cellsPerSide; i++) {
+        let row = document.createElement('div');
+        row.style.display = 'flex';
+
+        for(let j = 0; j < cellsPerSide; j++){
+            let cell = document.createElement('div');
+            cell.style.width = cellWidth + 'px';
+            cell.style.height = cellWidth + 'px';
             row.appendChild(cell);
         }
+
         container.appendChild(row);
     }
 }
 
-grid16x16();
+grid(100);
