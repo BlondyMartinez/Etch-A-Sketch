9b10ddef15160dc1f2237d5bfaa55d3ffcbd3b16
BlondyMartinez <blondymartinezm@gmail.com>
2024-02-12 05:30:49 +0100

    UI + random color

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 function grid(cellsPerSide) {
     let container = document.querySelector('#grid');
 
-    let gridWidth = (window.innerWidth * 0.8 / (16/9));
+    //clears grid
+    container.replaceChildren();
+
+    //80% of window's width divided by aspect ratio, this ensures the grid is always visible
+    let gridWidth = (window.innerWidth * 0.7 / (16/9)); 
     let cellWidth = gridWidth / cellsPerSide;
 
     container.style.height = gridWidth + 'px';
@@ -12,13 +16,57 @@ function grid(cellsPerSide) {
 
         for(let j = 0; j < cellsPerSide; j++){
             let cell = document.createElement('div');
+            cell.classList.add("cell");
             cell.style.width = cellWidth + 'px';
             cell.style.height = cellWidth + 'px';
+            cell.style.backgroundColor = 'white';
             row.appendChild(cell);
         }
 
         container.appendChild(row);
     }
+    
+    onHover("RANDOM_COLOR");
 }
 
-grid(100);
+function onHover(behavior){
+    let cellList = document.querySelectorAll(".cell");
+
+    for(let cell of cellList) {
+        cell.addEventListener('mouseover', function(){
+            switch(behavior) {
+                case "RANDOM_COLOR":
+                    cell.style.backgroundColor = getRandomColor();
+                    break;
+            }
+        });
+    }
+}
+
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+
+    let rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+    return rgb;
+}
+
+function clearGrid() {
+    let cellList = document.querySelectorAll(".cell");
+
+    for (let cell of cellList){
+        cell.style.backgroundColor = 'white';
+    }
+}
+
+function newGrid(){
+    let cells = prompt("How many cells per size do you want? Choose between 1 and 100.");
+
+    if (!cells  || cells < 1) cells = 1;
+    else if (cells > 100) cells = 100;
+
+    grid(cells);
+}
+
+grid(16);
